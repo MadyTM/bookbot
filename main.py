b9152912402df0476f1df7e0b01cc2f8eb945e1a
MadyTM <peta.madara@seznam.cz>
2025-08-28 13:51:38 +0200

    update main.py, stats.py to work with any .txt

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 from stats import get_num_words, get_num_chars, sort_char_counts
+import sys
 
 def get_book_text(filepath):
     with open(filepath) as f:
         return f.read()
 
 def main():
-    book_text = get_book_text("books/frankenstein.txt")
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_text = get_book_text(sys.argv[1])
     count = get_num_words(book_text)
     char_count = get_num_chars(book_text)
     sorted_counts = sort_char_counts(char_count)
     print("============ BOOKBOT ============")
-    print(f"Analyzing book found at books/frankenstein.txt")
+    print(f"Analyzing book found at {sys.argv[1]}")
     print("----------- Word Count ----------")
     print(f"Found {count} total words")
     print("--------- Character Count -------")
